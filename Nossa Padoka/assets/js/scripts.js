de854b4244e0b1349e688afcafa6567cd19f3c90
MazuttiGuilherme <91494392+MazuttiGuilherme@users.noreply.github.com>
2022-03-12 13:31:42 -0300

    Revisão Altera quantidade e preço no carrinhho

--- Nossa Padoka/assets/js/scripts.js
+++ Nossa Padoka/assets/js/scripts.js
@@ -76,9 +76,9 @@ const addToCart = (newProduct) => {
   }
   handleCartUpdate();
 };
-const removeOfCart = id => {
+const removeOfCart = (id) => {
   productsCart = productsCart.filter((product) => {
-    if (product.id === id) {
+    if (product.id == id) {
       return false;
     }
     return true;
@@ -89,8 +89,6 @@ const removeOfCart = id => {
   }
 };
 const updateItemQty = (id, newQty) => {
-  const newQtyNumber = parseInt(newQty)
-  if(isNaN(newQtyNumber > 0)) {
   const productIndex = productsCart.findIndex((product) => {
     if (product.id === id) {
       return true;
@@ -99,82 +97,88 @@ const updateItemQty = (id, newQty) => {
   });
   productsCart[productIndex].qty = parseInt(newQty);
   handleCartUpdate();
-  }
 };
 
 const handleCartUpdate = (renderItens = true) => {
   // Salva carrinho no localstorage
-  const productsCartString = JSON.stringify(productsCart)
-  localStorage.setItem('productsCart', productsCartString)
-  const emptyCartEl = document.querySelector('#empty-cart')
-  const cartWithProductsEl = document.querySelector('#cart-with-products')
-  const cartProductsListEl = cartWithProductsEl.querySelector('ul')
-  const cartBadgeEl = document.querySelector('.btn-cart-badge')
+  const productsCartString = JSON.stringify(productsCart);
+  localStorage.setItem("productsCart", productsCartString);
+  const emptyCartEl = document.querySelector("#empty-cart");
+  const cartWithProductsEl = document.querySelector("#cart-with-products");
+  const cartProductsListEl = cartWithProductsEl.querySelector("ul");
+  const cartBadgeEl = document.querySelector(".btn-cart-badge");
   if (productsCart.length > 0) {
     // Calcula totais
-    let total = 0
-    let totalPrice = 0
-    productsCart.forEach(product => {
-      total = total + product.qty
-      totalPrice = totalPrice + product.price * product.qty
-    })
+    let total = 0;
+    let totalPrice = 0;
+    productsCart.forEach((product) => {
+      total = total + product.qty;
+      totalPrice = totalPrice + product.price * product.qty;
+    });
     // Atualizar a badge
-    cartBadgeEl.classList.add('btn-cart-badge-show')
-    cartBadgeEl.textContent = total
+    cartBadgeEl.classList.add("btn-cart-badge-show");
+    cartBadgeEl.textContent = total;
     // Atualizo o total do carrinho
-    const cartTotalEl = document.querySelector('.cart-total p:last-child')
-    cartTotalEl.textContent = totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+    const cartTotalEl = document.querySelector(".cart-total p:last-child");
+    cartTotalEl.textContent = totalPrice.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
     // Exibir carrinho com produtos
-    cartWithProductsEl.classList.add('cart-with-products-show')
-    emptyCartEl.classList.remove('empty-cart-show')
+    cartWithProductsEl.classList.add("cart-with-products-show");
+    emptyCartEl.classList.remove("empty-cart-show");
     // Exibir produtos do carrinho na tela
     if (renderItens) {
-      cartProductsListEl.innerHTML = ''
+      cartProductsListEl.innerHTML = "";
       productsCart.forEach((product) => {
-        const listItemEl = document.createElement('li')
+        const listItemEl = document.createElement("li");
         listItemEl.innerHTML = `
-          <img src="${product.image}" alt="${product.name}" width="70" height="70" />
+          <img src="${product.image}" alt="${
+          product.name
+        }" width="70" height="70" />
           <div>
             <p class="h3">${product.name}</p>
-            <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2 })}</p>
+            <p class="price">R$ ${product.price.toLocaleString("pt-br", {
+              minimumFractionDigits: 2,
+            })}</p>
           </div>
           <input class="form-input" type="number" value="${product.qty}" />
           <button>
             <i class="fa-solid fa-trash-can"></i>
           </button>
-        `
-        const btnRemoveEl = listItemEl.querySelector('button')
-        btnRemoveEl.addEventListener('click', () => {
-          removeOfCart(product.id)
-        })
-        const inputQtyEl = listItemEl.querySelector('input')
-        inputQtyEl.addEventListener('keyup', (event) => {
-          updateItemQty(product.id, event.target.value)
-        })
-        inputQtyEl.addEventListener('keydown', (event) => {
-          if (event.key === '-' || event.key === '.' || event.key === ',') {
-            event.preventDefault()
+        `;
+        const btnRemoveEl = listItemEl.querySelector("button");
+        btnRemoveEl.addEventListener("click", () => {
+          removeOfCart(product.id);
+        });
+        const inputQtyEl = listItemEl.querySelector("input");
+        inputQtyEl.addEventListener("keyup", (event) => {
+          updateItemQty(product.id, event.target.value);
+        });
+        inputQtyEl.addEventListener("keydown", (event) => {
+          if (event.key === "-" || event.key === "." || event.key === ",") {
+            event.preventDefault();
           }
-        })
-        inputQtyEl.addEventListener('change', (event) => {
-          updateItemQty(product.id, event.target.value)
-        })
-        cartProductsListEl.appendChild(listItemEl)
-      })
+        });
+        inputQtyEl.addEventListener("change", (event) => {
+          updateItemQty(product.id, event.target.value);
+        });
+        cartProductsListEl.appendChild(listItemEl);
+      });
     }
   } else {
     // Esconder badge
-    cartBadgeEl.classList.remove('btn-cart-badge-show')
+    cartBadgeEl.classList.remove("btn-cart-badge-show");
     // Exibir carrinho vazio
-    emptyCartEl.classList.add('empty-cart-show')
-    cartWithProductsEl.classList.remove('cart-with-products-show')
+    emptyCartEl.classList.add("empty-cart-show");
+    cartWithProductsEl.classList.remove("cart-with-products-show");
   }
-}
-handleCartUpdate()
+};
+handleCartUpdate();
 // Atualiza carrinho se outra aba
-window.addEventListener('storage', (event) => {
-  if (event.key === 'productsCart') {
-    productsCart = JSON.parse(event.newValue)
-    handleCartUpdate()
+window.addEventListener("storage", (event) => {
+  if (event.key === "productsCart") {
+    productsCart = JSON.parse(event.newValue);
+    handleCartUpdate();
   }
-})
+});
